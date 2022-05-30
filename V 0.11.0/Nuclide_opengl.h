f837fdbf342989b2f2f02ca719fce496b28e2c1e
Erick Andrade <erickspace11@hotmail.com>
2022-05-30 12:47:59 -0300

    Creating V0.12.0

--- V 0.11.0/Nuclide_opengl.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPENGL
-#define OPENGL
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#endif // !OPENGL
-
---
+++ V 0.12.0/Core/Atom/Atom_shader.h
@@ -0,0 +1,49 @@
+#ifndef LIGHTNINGSHADER_H
+#define LIGHTNINGSHADER_H
+//OpenGL
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+//standard libary
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	// the program ID
+	unsigned int ID = 0;
+	// constructor reads and builds the shader
+	Shader(const char* vertexPath, const char* fragmentPath);
+	// use/activate the shader
+	void use();
+	//Set Uniforms bool
+	void setBool(const std::string& name, bool value) const;
+	//Set Uniforms int
+	void setInt(const std::string& name, int value) const;
+	//Set Uniforms float
+	void setFloat(const std::string& name, float value) const;
+	//Set Uniforms in float X, float y
+	void setVec2(const std::string& name, float x, float y) const;
+	//Set Uniforms in Vec2
+	void setVec2(const std::string& name, const glm::vec2& value) const;
+	//Set Uniforms in float X, float y, float z
+	void setVec3(const std::string& name, float x, float y, float z) const;
+	//Set Uniforms in Vec3
+	void setVec3(const std::string& name, const glm::vec3& value) const;
+	//Set Uniforms in Float x, float y, float z, float w
+	void setVec4(const std::string& name, float x, float y, float z, float w) const;
+	//Set Uniforms in Vec4
+	void setVec4(const std::string& name, const glm::vec4& value) const;
+	//Set Uniforms in Matrix 2x2
+	void setMat2(const std::string& name, const glm::mat2& mat2) const;
+	//Set Uniforms in Matrix 3x3
+	void setMat3(const std::string& name, const glm::mat3& mat3) const;
+	//Set Uniforms in Matrix 4x4
+	void setMat4(const std::string& name, const glm::mat4& mat4) const;
+private:
+	void checkCompileErros(GLint shader, std::string type);
+	std::string toArray(std::string ftext, std::string stext, int number);
+};
+#endif
